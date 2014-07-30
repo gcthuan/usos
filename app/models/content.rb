690fbccd54b8d819f3f5c30451e94b97d5a05a7f
altair <gcthuan@apcs.vn>
2014-07-30 17:29:31 +0700

    Add ignored list to Content

--- app/models/content.rb
+++ app/models/content.rb
@@ -6,9 +6,9 @@ class Content < ActiveRecord::Base
   accepts_nested_attributes_for :user_info
   serialize :ignored_list, Array
 
-  def find_nearby_devices latitude, longitude, radius, device_token, username, ignored_list
-    puts ignored_list
-  	device_list = Device.near([latitude, longitude], radius, units: :km)
+  def find_nearby_devices content, radius
+    puts content.ignored_list
+  	device_list = Device.near([content.latitude, content.longitude], radius, units: :km)
     token_list = Array.new
     device_list.each do |device|
       token_list << device.device_token
@@ -20,21 +20,21 @@ class Content < ActiveRecord::Base
     #if ignore_list.nil?
     #  token_list = token_list - [device_token]
     #else
-      token_list = token_list - ignored_list - [device_token]
+      token_list = token_list - content.ignored_list - [device_token]
     #end
     token_list.each do |token|
       puts token
       #APNS.send_notification(token.to_s, alert: "#{username} needs your help!", badge: 1, sound: 'default', :other => {:sent => 'with apns gem', :custom_param => "value"})
     end
-    ignored_list = token_list
+    content.update_attribute :ignored_list :token_list
   end
 
   def broadcast
-    delay(run_at: 0.minute.from_now.utc).find_nearby_devices self.latitude, self.longitude, 1, self.device_token, self.user_info.name, self.ignored_list
-  	delay(run_at: 1.minute.from_now.utc).find_nearby_devices self.latitude, self.longitude, 2, self.device_token, self.user_info.name, self.ignored_list
-  	delay(run_at: 2.minute.from_now.utc).find_nearby_devices self.latitude, self.longitude, 3, self.device_token, self.user_info.name, self.ignored_list
-  	delay(run_at: 3.minute.from_now.utc).find_nearby_devices self.latitude, self.longitude, 5, self.device_token, self.user_info.name, self.ignored_list
-  	delay(run_at: 4.minute.from_now.utc).find_nearby_devices self.latitude, self.longitude, 8, self.device_token, self.user_info.name, self.ignored_list
+    delay(run_at: 0.minute.from_now.utc).find_nearby_devices self, 1
+  	delay(run_at: 1.minute.from_now.utc).find_nearby_devices self, 2
+  	delay(run_at: 2.minute.from_now.utc).find_nearby_devices self, 3
+  	delay(run_at: 3.minute.from_now.utc).find_nearby_devices self, 5
+  	delay(run_at: 4.minute.from_now.utc).find_nearby_devices self, 8
     if self.status == 'available'
       self.delay(run_at: 60.minutes.from_now.utc).update_attribute :status, "expired"
     end

4d048afd26ba813cb7f8570235f9e15b425fdbbe
altair <gcthuan@apcs.vn>
2014-07-30 15:59:10 +0700

    Fix notification

--- app/models/content.rb
+++ app/models/content.rb
@@ -22,6 +22,7 @@ class Content < ActiveRecord::Base
     end
     previous_list = device_list
     token_list.each do |token|
+      puts token
       APNS.send_notification(token.to_s, alert: "#{username} needs your help!", badge: 1, sound: 'default', :other => {:sent => 'with apns gem', :custom_param => "value"})
     end
   end

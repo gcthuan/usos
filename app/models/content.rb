4f2508013ebf87aaf992d2e1cb6d861bcb418824
altair <gcthuan@apcs.vn>
2014-07-30 16:28:23 +0700

    Fix previous list

--- app/models/content.rb
+++ app/models/content.rb
@@ -6,7 +6,7 @@ class Content < ActiveRecord::Base
   accepts_nested_attributes_for :user_info
   attr_accessor :previous_list
 
-  def find_nearby_devices latitude, longitude, radius, device_token, username
+  def find_nearby_devices latitude, longitude, radius, device_token, username, ignore_list
     puts previous_list
   	device_list = Device.near([latitude, longitude], radius, units: :km)
     token_list = Array.new
@@ -17,26 +17,27 @@ class Content < ActiveRecord::Base
   	puts "#{token_list}"
   	puts "---------------------------------------------------"
 
-    if previous_list.nil?
-      token_list = token_list - [device_token]
-    else
-      token_list = token_list - previous_list - [device_token]
-    end
-    previous_list = device_list
-    puts previous_list
+    #if ignore_list.nil?
+    #  token_list = token_list - [device_token]
+    #else
+      token_list = token_list - ignore_list - [device_token]
+    #end
+    
     puts "--------------------------------------"
     token_list.each do |token|
       puts token
       #APNS.send_notification(token.to_s, alert: "#{username} needs your help!", badge: 1, sound: 'default', :other => {:sent => 'with apns gem', :custom_param => "value"})
     end
+    previous_list = token_list
   end
 
   def broadcast
-  	delay(run_at: 0.minute.from_now.utc).find_nearby_devices self.latitude, self.longitude, 1, self.device_token, self.user_info.name
-  	delay(run_at: 1.minute.from_now.utc).find_nearby_devices self.latitude, self.longitude, 2, self.device_token, self.user_info.name
-  	delay(run_at: 2.minute.from_now.utc).find_nearby_devices self.latitude, self.longitude, 3, self.device_token, self.user_info.name
-  	delay(run_at: 3.minute.from_now.utc).find_nearby_devices self.latitude, self.longitude, 5, self.device_token, self.user_info.name
-  	delay(run_at: 4.minute.from_now.utc).find_nearby_devices self.latitude, self.longitude, 8, self.device_token, self.user_info.name
+    previous_list = Array.new
+  	previous_list = delay(run_at: 0.minute.from_now.utc).find_nearby_devices self.latitude, self.longitude, 1, self.device_token, self.user_info.name, previous_list
+  	previous_list = delay(run_at: 1.minute.from_now.utc).find_nearby_devices self.latitude, self.longitude, 2, self.device_token, self.user_info.name, previous_list
+  	previous_list = delay(run_at: 2.minute.from_now.utc).find_nearby_devices self.latitude, self.longitude, 3, self.device_token, self.user_info.name, previous_list
+  	previous_list = delay(run_at: 3.minute.from_now.utc).find_nearby_devices self.latitude, self.longitude, 5, self.device_token, self.user_info.name, previous_list
+  	previous_list = delay(run_at: 4.minute.from_now.utc).find_nearby_devices self.latitude, self.longitude, 8, self.device_token, self.user_info.name, previous_list
     if self.status == 'available'
       self.delay(run_at: 60.minutes.from_now.utc).update_attribute :status, "expired"
     end
